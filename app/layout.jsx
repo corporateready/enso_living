cdde1245509a5a97800499f4b9ca2fb388929b8f
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-01-26 11:12:54 +0200

    correction of animate elements updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
           href="/favicon_io/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon_io/site.webmanifest" />
-        {/* <GoogleTagManager gtmId={GTM_ID} />
+        <GoogleTagManager gtmId={GTM_ID} />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
@@ -78,7 +78,7 @@ export default function RootLayout({ children }) {
             </iframe>
             `,
           }}
-        /> */}
+        />
       </head>
       <body className={`antialiased`}>
         {children}
