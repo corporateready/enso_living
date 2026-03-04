9df0e079b53ddbaaf886defde1e3cfcccaf17a78
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-03-04 12:17:12 +0200

    fixed logo animation mobie last version second updated

--- app/components/developer/mobile-logo/index.jsx
+++ app/components/developer/mobile-logo/index.jsx
@@ -226,7 +226,7 @@ export const Logo = () => {
                A 36 36 0 0 1 149 78
                A 36 36 0 0 1 113 114"
               stroke="white"
-              strokeWidth={10}
+              strokeWidth={20}
               strokeLinecap="round"
               fill="none"
               variants={{
